4c9f4e1f2c21e11463fb8358717eba12d4c2c1d1
Syed Bazil Zaidi <zaidibazil24@gmail.com>
2026-08-07 02:48:57 +0500

    Resolved CodeRabbit Reviews

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,33 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const User = require('../models/User');
-
-// const protect = async (req, res, next) => {
-//   try {
-//     const authHeader = req.headers.authorization;
-
-//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//       return res.status(401).json({ message: 'Not authorized, no token' });
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     const user = await User.findById(decoded.id).select('-password');
-//     if (!user) {
-//       return res.status(401).json({ message: 'User no longer exists' });
-//     }
-
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     req.log.error(error, 'Authentication failed');
-//     res.status(401).json({ message: 'Not authorized, invalid token' });
-//   }
-// };
-
-// module.exports = protect;
-
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
